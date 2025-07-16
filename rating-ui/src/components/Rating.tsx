338ce604738a0a421dfd6cf646909181457e855e
nbrach <nbrach@pop-os.localdomain>
2025-07-15 22:40:10 -0700

    add kind of dynamic array to hold # of stars, adds state for clicking + hovering over star, adds message after selecting star

--- rating-ui/src/components/Rating.tsx
+++ rating-ui/src/components/Rating.tsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+  const feedbackMessages = ["Poor", "Fair", "Good", "Very Good", "Excellent"];
+
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
+      <div className="stars">
+        {stars.map((star) => (
+          <span
+            className={`star ${star <= (hover || rating) ? "active" : ""}`}
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+          >
+            {"\u2605"} {/* Unicode star character */}
+          </span>
+        ))}
+      </div>
+      <div className="feedback">
+        {rating > 0 && (
+          <p>
+            <strong>{feedbackMessages[rating - 1]}</strong>
+          </p>
+        )}
+      </div>
     </div>
   );
 };
